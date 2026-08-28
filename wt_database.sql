efb03076384e97c6cede929e22cb7821d8762776
Sadusdel <127667524+Sadusdel@users.noreply.github.com>
2026-08-28 20:45:52 +0300

    Align database dump with appointment slot schema

--- wt_database.sql
+++ wt_database.sql
@@ -1,51 +1,46 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 05, 2017 at 05:53 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Appointment Booking System database
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+SET NAMES utf8mb4;
 
 --
 -- Database: `wt_database`
 --
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `book`
 --
 
 CREATE TABLE `book` (
+  `appointment_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `Username` varchar(30) NOT NULL,
   `Fname` varchar(30) NOT NULL,
   `Gender` varchar(10) NOT NULL,
   `CID` int(5) NOT NULL,
   `DID` int(5) NOT NULL,
   `DOV` date NOT NULL,
+  `appointment_time` time NOT NULL,
   `Timestamp` datetime NOT NULL,
-  `Status` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `Status` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `active_slot_key` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`Username`,`Fname`,`DOV`,`Timestamp`),
+  UNIQUE KEY `appointment_id` (`appointment_id`),
+  UNIQUE KEY `unique_active_slot` (`active_slot_key`),
+  KEY `idx_book_doctor_date_time` (`DID`,`CID`,`DOV`,`appointment_time`),
+  KEY `idx_book_patient_date` (`Username`,`DOV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `book`
 --
 
-INSERT INTO `book` (`Username`, `Fname`, `Gender`, `CID`, `DID`, `DOV`, `Timestamp`, `Status`) VALUES
-('user', 'patient', 'male', 1, 1, '2017-11-08', '2017-11-05 16:43:48', 'Booking Registered.Wait for the update');
+INSERT INTO `book` (`Username`, `Fname`, `Gender`, `CID`, `DID`, `DOV`, `appointment_time`, `Timestamp`, `Status`, `active_slot_key`) VALUES
+('user', 'patient', 'male', 1, 1, '2017-11-08', '00:00:00', '2017-11-05 16:43:48', 'Booking Registered.Wait for the update', NULL);
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `clinic`
 --
@@ -60,15 +55,10 @@ CREATE TABLE `clinic` (
   `mid` varchar(5) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `clinic`
---
-
 INSERT INTO `clinic` (`cid`, `name`, `address`, `town`, `city`, `contact`, `mid`) VALUES
 (1, 'Clinic', 'XYZ apartment, CST', 'CST', 'Mumbai', 9999988888, '1');
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `doctor`
 --
@@ -87,15 +77,10 @@ CREATE TABLE `doctor` (
   `region` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `doctor`
---
-
 INSERT INTO `doctor` (`did`, `name`, `gender`, `dob`, `experience`, `specialization`, `contact`, `address`, `username`, `password`, `region`) VALUES
 (1, 'doctor', 'male', '1980-01-01', 10, 'Orthodontist', 9999999999, 'XYZ tower, CST', 'doctor', 'doctor', 'Mumbai');
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `doctor_availability`
 --
@@ -108,10 +93,6 @@ CREATE TABLE `doctor_availability` (
   `endtime` time NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `doctor_availability`
---
-
 INSERT INTO `doctor_availability` (`cid`, `did`, `day`, `starttime`, `endtime`) VALUES
 (1, 1, 'Friday', '14:00:00', '18:00:00'),
 (1, 1, 'Monday', '14:00:00', '18:00:00'),
@@ -120,7 +101,6 @@ INSERT INTO `doctor_availability` (`cid`, `did`, `day`, `starttime`, `endtime`) 
 (1, 1, 'Wednesday', '14:00:00', '18:00:00');
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `manager`
 --
@@ -137,15 +117,10 @@ CREATE TABLE `manager` (
   `region` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `manager`
---
-
 INSERT INTO `manager` (`mid`, `name`, `gender`, `dob`, `contact`, `address`, `username`, `password`, `region`) VALUES
 (1, 'Manager', 'male', '1990-01-01', 8888899999, 'XYZ complex CST', 'manager', 'manager', 'Mumbai');
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `manager_clinic`
 --
@@ -155,15 +130,10 @@ CREATE TABLE `manager_clinic` (
   `mid` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `manager_clinic`
---
-
 INSERT INTO `manager_clinic` (`cid`, `mid`) VALUES
 (1, 1);
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `patient`
 --
@@ -178,59 +148,5 @@ CREATE TABLE `patient` (
   `password` varchar(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `patient`
---
-
 INSERT INTO `patient` (`name`, `gender`, `dob`, `contact`, `email`, `username`, `password`) VALUES
 ('user', 'male', '1985-01-01', 7897897897, 'user@test.com', 'user', 'user');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`Username`,`Fname`,`DOV`,`Timestamp`);
-
---
--- Indexes for table `clinic`
---
-ALTER TABLE `clinic`
-  ADD PRIMARY KEY (`cid`,`name`);
-
---
--- Indexes for table `doctor`
---
-ALTER TABLE `doctor`
-  ADD PRIMARY KEY (`did`);
-
---
--- Indexes for table `doctor_availability`
---
-ALTER TABLE `doctor_availability`
-  ADD PRIMARY KEY (`cid`,`did`,`day`,`starttime`,`endtime`);
-
---
--- Indexes for table `manager`
---
-ALTER TABLE `manager`
-  ADD PRIMARY KEY (`mid`);
-
---
--- Indexes for table `manager_clinic`
---
-ALTER TABLE `manager_clinic`
-  ADD PRIMARY KEY (`cid`,`mid`);
-
---
--- Indexes for table `patient`
---
-ALTER TABLE `patient`
-  ADD PRIMARY KEY (`email`,`username`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
